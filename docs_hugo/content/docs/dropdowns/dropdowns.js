68ea7c2a9f0ec7086ceba9a2bfda05689abd1dc3
Studio KonKon <45437528+StudioKonKon@users.noreply.github.com>
2021-03-23 21:21:05 +0000

    dropdown and tables updates

---
+++ docs_hugo/content/docs/dropdowns/dropdowns.js
@@ -0,0 +1,24 @@
+"use strict";
+(function() {
+	var $html = document.querySelector("html"),
+		forms = [];
+	
+	document
+		.querySelectorAll(".be-content .main-content form")
+		.forEach(function(forms) {
+			forms.setAttribute("action", location.href + "#");
+			forms.addEventListener("submit", function(e) {
+				e.preventDefault();
+				return false;
+			});
+		});
+	
+	document
+		.querySelectorAll(".be-content .main-content a.dropdown-item")
+		.forEach(function(links) {
+			links.addEventListener("click", function (event) {
+				event.preventDefault();
+			});
+		});
+	
+})();
