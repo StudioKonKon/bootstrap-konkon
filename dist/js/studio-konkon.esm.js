99639be87870eedb601b34f83f6e80afe1e2603f
Studio KonKon <45437528+StudioKonKon@users.noreply.github.com>
2026-01-18 18:35:59 +0000

    update#260119033447

--- dist/js/studio-konkon.esm.js
+++ dist/js/studio-konkon.esm.js
@@ -1,6 +1,6 @@
 /*!
   * Studio KonKon v1.14.3-beta (https://play.studiokonkon.com/)
-  * Copyright 2011-2025 Studio KonKon
+  * Copyright 2011-2026 Studio KonKon
   * Licensed under MIT (https://github.com/StudioKonKon/bootstrap-konkon/blob/master/LICENSE)
   */
 /**
